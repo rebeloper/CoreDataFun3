3c4446a57dd4c0dfeca5ae27e2c2e37f4428d388
Alex Nagy <therebeloper@gmail.com>
2020-08-11 10:54:08 +0300

    Initial Commit

---
+++ CoreDataFun3/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CoreDataFun3
+//
+//  Created by Alex Nagy on 11/08/2020.
+//  Copyright © 2020 Alex Nagy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
